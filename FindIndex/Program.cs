52b28931aec0a7ee246e6a418d8874104a128b68
Constantine <rooki12@mail.ru>
2022-12-21 21:51:07 +0300

    Реализация программы

---
+++ FindIndex/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует
+//  массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести
+//  с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+
+System.Console.WriteLine("Введите строки через пробел: ");
+string UsrStr = Console.ReadLine();
+string[] splt = UsrStr.Split(' ');
+string[] array2 = new string[splt.Length];
+
+void SecondArrayWithIF(string[] splt1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < splt.Length; i++)
+    {
+    if(splt[i].Length <= 3)
+        {
+        array2[count] = splt[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayWithIF(splt, array2);
+PrintArray(array2);
